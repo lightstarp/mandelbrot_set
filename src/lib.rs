c560b23e32a1281ac4cedcb158add9d81382434d
lightstarp <naokumeyokohama412@gmail.com>
2026-01-23 11:36:30 +0900

    change of high_performance

--- src/lib.rs
+++ src/lib.rs
@@ -225,7 +225,7 @@ impl State {
 
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
+                power_preference: wgpu::PowerPreference::HighPerformance,
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
             })
